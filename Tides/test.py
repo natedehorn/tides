f067b005514d161c5800e8fcbfc5c5e80c67bff1
natedehorn <natedehorn@gmail.com>
2016-10-23 14:24:15 -0400

    Updating unit tests
    Revamped tests to test functionality

--- Tides/test.py
+++ Tides/test.py
@@ -2,57 +2,128 @@ import tc
 import datetime
 import unittest
 
-class TestTidesMethods(unittest.TestCase):
-	def test_station(self):
-		char_harb = tc.Station('SCarolina', '8665530')
-		self.assertEqual(str(char_harb),'SCarolina, 8665530')
-		self.assertEqual(str(char_harb.site),'SCarolina')
-		self.assertEqual(str(char_harb.station_number),'8665530')
+class TestDate(unittest.TestCase):
+	def test_repr(self):
+		TEST_DATE = tc.Date(1, 2, 2016)
+		self.assertEqual(str(TEST_DATE), '1 2 2016')
 
-	def test_tides(self):
-		char_harb = tc.Station('SCarolina', '8665530')
-		char_harb_tides = tc.Tides(char_harb, tc.Date('1', '2016'))
-		self.assertIs(type(char_harb_tides),tc.Tides)
+	def test_attributes(self):
+		TEST_DATE = tc.Date(4, 20, 2015)
+		self.assertEqual(TEST_DATE.day, 4)
+		self.assertEqual(TEST_DATE.month, 20)
+		self.assertEqual(TEST_DATE.year, 2015)
 
-	def testplot(self):
-		char_harb = tc.Station('SCarolina', '8665530')
-		char_harb_tides = tc.Tides(char_harb, tc.Date('1', '2016'))
-		self.assertEqual(str(char_harb_tides.graph), char_harb.site + '.png')
+	def test_type(self):
+		TODAY = tc.Date(datetime.datetime.now().day, datetime.datetime.now().month, datetime.datetime.now().year)
+		self.assertIs(type(TODAY), tc.Date)
 
-class TestEmailMethods(unittest.TestCase):
-	def test_basic_email(self):
-		email = tc.Email('tidesbot@gmail.com','tides4all', '5742989709@mms.att.net', 'Test')
-		self.assertEqual(str(email),'Sender : tidesbot@gmail.com\nPassword: tides4all\nRecipient: 5742989709@mms.att.net\nBody: Test')
+class TestStation(unittest.TestCase):
+	def test_repr(self):
+		TEST_STATION = tc.Station('SCarolina', '8665530')
+		self.assertEqual(str(TEST_STATION), 'SCarolina, 8665530')
 
-	def test_attachment_email(self):
-		email = tc.Email('tidesbot@gmail.com','tides4all', '5742989709@mms.att.net', 'Test')
-		email.attach('Test.png', 'Test.png')
-		self.assertEqual(str(email),'Sender : tidesbot@gmail.com\nPassword: tides4all\nRecipient: 5742989709@mms.att.net\nBody: Test\nFilename: Test.png\nAttachment: Test.png')
+	def test_attributes(self):
+		TEST_STATION = tc.Station('SCarolina', '8665530')
+		self.assertEqual(str(TEST_STATION.site),'SCarolina')
+		self.assertEqual(str(TEST_STATION.station_number),'8665530')
 
-	def test_subject_email(self):
-		email = tc.Email('tidesbot@gmail.com','tides4all', '5742989709@mms.att.net', 'Test')
-		email.setSubject('Test Subject')
-		self.assertEqual(str(email),'Sender : tidesbot@gmail.com\nPassword: tides4all\nRecipient: 5742989709@mms.att.net\nBody: Test\nSubject: Test Subject')
+	def test_type(self):
+		TEST_STATION = tc.Station('SCarolina', '8665530')
+		self.assertIs(type(TEST_STATION), tc.Station)
 
-	def test_complete_email(self):
-		email = tc.Email('tidesbot@gmail.com','tides4all', '5742989709@mms.att.net', 'Test')
-		email.setSubject('Test Subject')
-		email.attach('Test.png', 'Test.png')
-		self.assertEqual(str(email),'Sender : tidesbot@gmail.com\nPassword: tides4all\nRecipient: 5742989709@mms.att.net\nBody: Test\nSubject: Test Subject\nFilename: Test.png\nAttachment: Test.png')
+class TestTide(unittest.TestCase):
+	def test_repr(self):
+		TEST_TIDE = tc.Tide('High', '4:20', 'PM', '4.2')
+		self.assertEqual(str(TEST_TIDE), 'High, 4:20, PM, 4.2')
+
+	def test_attributes(self):
+		TEST_TIDE = tc.Tide('Low', '10:40', 'AM', '-0.2')
+		self.assertEqual(TEST_TIDE.phase, 'Low')
+		self.assertEqual(TEST_TIDE.time, '10:40')
+		self.assertEqual(TEST_TIDE.period, 'AM')
+		self.assertEqual(TEST_TIDE.level, '-0.2')
+
+	def test_type(self):
+		TEST_TIDE = tc.Tide('High', '12:10', 'AM', 's')
+		self.assertIs(type(TEST_TIDE), tc.Tide)
+
+class TestTides(unittest.TestCase):
+	def test_attributes(self):
+		TEST_STATION = tc.Station('SCarolina', '8665530')
+		TODAY = tc.Date(datetime.datetime.now().day, datetime.datetime.now().month, datetime.datetime.now().year)
+		TEST_TIDES = tc.Tides(TEST_STATION, TODAY)
+		self.assertEqual(str(TEST_TIDES.station), str(tc.Station('SCarolina', '8665530')))
+		self.assertEqual(str(TEST_TIDES.date), str(TODAY))
+		self.assertTrue(hasattr(TEST_TIDES, 'tides'))
+		self.assertTrue(hasattr(TEST_TIDES, 'graph'))
 		
-	def test_send_complete_email(self):
-		now = datetime.datetime.now()
-		charleston = tc.Station('SCarolina', '8665099')
-		station = charleston
-		tides = tc.Tides(station, tc.Date(now.month,now.year))
-		emailadd = 'tidesbot@gmail.com'
-		password = 'tides4all'
-		phoneadd = '5742989709@mms.att.net'
-		emailbod = str(tides.tides)
-		email = tc.Email(emailadd, password, phoneadd, emailbod)
-		email.setSubject('Tides for ' + str(now))
-		email.attach(tides.station.site + '.png', tides.graph)
-		email.send()
+	def test_type(self):
+		TEST_STATION = tc.Station('SCarolina', '8665530')
+		TODAY = tc.Date(datetime.datetime.now().day, datetime.datetime.now().month, datetime.datetime.now().year)
+		TEST_TIDES = tc.Tides(TEST_STATION, TODAY)
+		self.assertIs(type(TEST_TIDES), tc.Tides)
+
+class TestEmail(unittest.TestCase):
+	def test_basic_repr(self):
+		with open('email.info','r+') as FILE:
+			TEST_INFO = FILE.readlines()
+		TEST_EMAIL = tc.Email(TEST_INFO[0], TEST_INFO[1], TEST_INFO[2], 'Test Subject', 'Test Body')
+		self.assertEqual(str(TEST_EMAIL), 'Sender : %s\nPassword: %s\nRecipient: %s\nSubject: %s\nBody: %s' % (TEST_INFO[0], TEST_INFO[1], TEST_INFO[2], 'Test Subject', 'Test Body'))
+
+	def test_basic_attributes(self):
+		with open('email.info','r+') as FILE:
+			TEST_INFO = FILE.readlines()
+		TEST_EMAIL = tc.Email(TEST_INFO[0], TEST_INFO[1], TEST_INFO[2], 'Test Subject', 'Test Body')
+		self.assertEqual(str(TEST_EMAIL.sender), TEST_INFO[0])
+		self.assertEqual(str(TEST_EMAIL.password), TEST_INFO[1])
+		self.assertEqual(str(TEST_EMAIL.recipient), TEST_INFO[2])
+		self.assertEqual(str(TEST_EMAIL.subject), 'Test Subject')
+		self.assertEqual(str(TEST_EMAIL.body), 'Test Body')
+
+	def test_basic_type(self):
+		with open('email.info','r+') as FILE:
+			TEST_INFO = FILE.readlines()
+		TEST_EMAIL = tc.Email(TEST_INFO[0], TEST_INFO[1], TEST_INFO[2], 'Test Subject', 'Test Body')
+		self.assertIs(type(TEST_EMAIL), tc.Email)
 		
+	def test_attach_repr(self):
+		with open('email.info','r+') as FILE:
+			TEST_INFO = FILE.readlines()
+		TEST_EMAIL = tc.Email(TEST_INFO[0], TEST_INFO[1], TEST_INFO[2], 'Test Subject', 'Test Body')
+		TEST_EMAIL.attach('Test.png', 'Test.png')
+		self.assertEqual(str(TEST_EMAIL), str('Sender : %s\nPassword: %s\nRecipient: %s\nSubject: Test Subject\nBody: Test Body\nFilename: Test.png\nAttachment: Test.png' % (TEST_INFO[0], TEST_INFO[1], TEST_INFO[2])))
+
+	def test_attach_attributes(self):
+		with open('email.info','r+') as FILE:
+			TEST_INFO = FILE.readlines()
+		TEST_EMAIL = tc.Email(TEST_INFO[0], TEST_INFO[1], TEST_INFO[2], 'Test Subject', 'Test Body')
+		TEST_EMAIL.attach('Test.png', 'Test.png')
+		self.assertEqual(str(TEST_EMAIL.sender), TEST_INFO[0])
+		self.assertEqual(str(TEST_EMAIL.password), TEST_INFO[1])
+		self.assertEqual(str(TEST_EMAIL.recipient), TEST_INFO[2])
+		self.assertEqual(str(TEST_EMAIL.subject), 'Test Subject')
+		self.assertEqual(str(TEST_EMAIL.body), 'Test Body')
+		self.assertEqual(str(TEST_EMAIL.filename), 'Test.png')
+		self.assertEqual(str(TEST_EMAIL.attachment), 'Test.png')
+
+	def test_attach_type(self):
+		with open('email.info','r+') as FILE:
+			TEST_INFO = FILE.readlines()
+		TEST_EMAIL = tc.Email(TEST_INFO[0], TEST_INFO[1], TEST_INFO[2], 'Test Subject', 'Test Body')
+		TEST_EMAIL.attach('Test.png', 'Test.png')
+		self.assertIs(type(TEST_EMAIL), tc.Email)
+
+	def test_send_email(self):
+		NOW = datetime.datetime.now()
+		DATE = tc.Date(NOW.day, NOW.month, NOW.year)
+		TIDES = tc.Tides(tc.Station('SCarolina', '8665099'), DATE)
+		SUBJECT = 'Tides for %s' % (str(DATE))
+		BODY = str(TIDES.tides)
+		with open('email.info','r+') as FILE:
+			INFO = FILE.readlines()
+		EMAIL = tc.Email(INFO[0], INFO[1], INFO[2], SUBJECT, BODY)
+		EMAIL.attach(TIDES.station.site + '.png', TIDES.graph)
+		EMAIL.send()
+
 if __name__ == '__main__':
 	unittest.main()
